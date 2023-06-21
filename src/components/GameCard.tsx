c59512203a9e8d12aaeb5e0a2a8eb60cb5b12856
jzees <my3ncheri08@gmail.com>
2023-06-21 00:10:03 -0400

    Show loading skeletons.

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { Game } from "../hooks/useGames";
 import { Card, CardBody, HStack, Heading, Image, Text } from "@chakra-ui/react";
 import PlatformIconList from "./PlatformIconList";
@@ -10,7 +9,7 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
+    <Card width="250px" borderRadius={10} overflow="hidden">
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
